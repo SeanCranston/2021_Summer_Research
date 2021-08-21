b8a7f4cfa7f91917ae37e06a879835dcb94ebd37
SeanCranston <seancranston0@gmail.com>
2021-08-21 14:20:24 -0500

    final?

--- Movie_recommender/app.R
+++ Movie_recommender/app.R
@@ -1,3 +1,6 @@
+####################################### Packages ###############################
+
+
 library(shiny)
 library(shinyWidgets)
 library(shinythemes)
@@ -5,12 +8,13 @@ library(DT)
 library(data.table)
 library(tidyverse)
 library(recommenderlab)
+library(xtable)
 
-################################################################################
-# Data importing
-setwd("C:/Users/seanc/OneDrive/Documents/2021_Summer_Research") #set working directory, will have to change if ran on another computer
-# should make a downloadable link so don't have to do this
-movie_data <- read_csv("movies.csv",
+
+#################################### Imports ###################################
+
+
+movie_data <- read_csv("https://raw.githubusercontent.com/SeanCranston/2021_Summer_Research/master/Data/movies.csv",
                        col_types = cols(
                            movieId = col_double(),
                            title = col_character(),
@@ -19,18 +23,25 @@ movie_data <- read_csv("movies.csv",
 i = 1
 movie_row <- movie_data[i,]
 
-rating_data <- read_csv("ratings.csv",
+rating_data <- read_csv("https://raw.githubusercontent.com/SeanCranston/2021_Summer_Research/master/Data/ratings.csv",
                         col_types = cols(
                             userId = col_double(),
                             movieId = col_double(),
                             rating = col_double(),
                             timestamp = col_double()
-                        )) %>% 
-    select(-timestamp)
+    #                     )) %>% 
+    # select(-timestamp)
+))
 
-################################################################################
+################################### user interface #############################
+
 
 ui = navbarPage("Movie Recommender",
+                
+                
+                ################## page one ####################################
+                
+                
                 tabPanel("Data Entry",fluidPage(theme = shinytheme("flatly")),
                          tags$head(
                              tags$style(HTML(".shiny-output-error-validation{color: red;}"))),
@@ -48,68 +59,120 @@ ui = navbarPage("Movie Recommender",
                              ),
                              
                              mainPanel(
-                                 dataTableOutput("movieID"),
+                                 uiOutput("movieID"),
+                                 #dataTableOutput("movieID"),
                                  hr(),
                                  dataTableOutput("table")
                              )
                          )
                 ),
                 
+                
+                ################## page two ####################################
+                
+                
                 tabPanel("Predicting movies",fluidPage(theme = shinytheme("flatly")),
                          tags$head(
                              tags$style(HTML(".shiny-output-error-validation{color: red;}"))),
                          pageWithSidebar(
                              headerPanel('Rank movies'),
-                             sidebarPanel(width = 4,
-                                          selectInput("model",
-                                                      "Select a model",
-                                                      c("Popular","ALS","UBCF Cosine","SVD"))
-                             ),
+                             sidebarPanel(width = 3,
+                                          
+                                          textInput('USERID', 'Enter user to be predicted',669),
+                                          
+                                          sliderTextInput(
+                                              inputId = "train_submit",
+                                              label = "What proportion of data should be in training dataset?", 
+                                              choices = seq(from = 0, to = 1, by = .05),
+                                              grid = TRUE,
+                                              selected = .8
+                                          ),
+                                          
+                                          pickerInput(inputId = "model",
+                                              #label = "Select a Model", 
+                                              choices = c("Popular","ALS","UBCF","SVD"),
+                                              options = list(title = "Select a model"),
+                                              choicesOpt = list(subtext = c("","","Cosine",""))),
+                                          
+                                          actionButton("pred_submit", "Make predictions")
+                                          ),
                              
                              mainPanel(
-                                 dataTableOutput("df")
-                             )
+                                 textOutput("Make sure to rate some movies"),
+                                 uiOutput("pred")
+                                 )
                          )
-                         
-                         
                 ),
-                
-                tabPanel("Developer",
-                         p(a("Sean Cranston", href="https://github.com/SeanCranston", target="_blank"),style = "font-size:25px"),
+                tabPanel("Info",
+                         p("Developer",style = "font-size:30px"),
+                         p(a("Sean Cranston", href="https://github.com/SeanCranston", target="_blank"),
+                           style = "font-size:25px"),
                          p("e-mail: seancranston0@gmail.com",style = "font-size:20px"),
-                )
-                
+                         hr(),
+                         p("Further Readings",style ="font-size:30px"),
+                         p("For more details on how we decided to use certain models 
+                           with certain paramaters please visit this github repository", 
+                           a("https://github.com/SeanCranston/2021_Summer_Research",
+                             href="https://github.com/SeanCranston/2021_Summer_Research"), "or the 
+                           knitted rmd that it producted", a("https://rpubs.com/Sean_Cranston/Movie_reco",
+                                                             href="https://rpubs.com/Sean_Cranston/Movie_reco"))
+                ) 
 )
 
-################################################################################
+
+########################################### server #############################
+
+
 server = function(input,output){
+    
+    
+    ##################################### Page one #############################
+    
+    
     
     ##### this outputs the a row from movie_data that is to be rated
     values <- reactiveValues(movie_row = movie_data[i,])
     row_index <- reactiveValues(i = 1)
+    
+    #### this allows the user to go forward and rate the next movie
     observeEvent(input$Next, {
-        if(row_index$i < 10329){#input$Nex>0 &&
+        if(row_index$i < 10329){
             row_index$i = row_index$i + 1
             temp <- movie_data[row_index$i,]
             values$movie_row <- temp
         }
     })
+    #### this allows the user to go back to the previous movie to be rated
     observeEvent(input$Previous,{
-        if(row_index$i > 1){#input$Prev>0 &&
+        if(row_index$i > 1){
             row_index$i = row_index$i - 1
             temp <- movie_data[row_index$i,]
             values$movie_row <- movie_data[row_index$i,]
         }
     })
-    output$movieID <- renderDT(values$movie_row)
-
-    ##### This outputs rating_data with the updated ratings from above
+    #### this controls how the matrix looks that the user is rating
+    output$movieID <- renderUI({
+        M <- values$movie_row %>% as.matrix()
+        M <- print(xtable(M, align = rep("c", ncol(M)+1)
+                          ),
+                   floating = F, tabular.environment = "array", comment = F, print.results = F
+                   )
+        html <- paste0("$$",M,"$$")
+        html %>% 
+            HTML() %>% 
+            withMathJax() %>% 
+            list()
+    })
+    
+    
+    ##### This outputs rating_data with the updated ratings from the user
     df_recactive <- reactiveValues()
     df <- eventReactive(input$goButton, {
         if(input$NewID!="" && !is.null(input$NewVal) && input$goButton>0){
             newrow = tibble(movieId = values$movie_row$movieId,
-                                rating = input$NewVal,
-                                userId = input$NewID)
+                            rating = input$NewVal,
+                            userId = input$NewID,
+                            timestamp = 0)
             rating_data <<- rbind(newrow, rating_data, fill = T) #use <<- if you want the user to save there input
         }
         rating_data %>% 
@@ -118,43 +181,70 @@ server = function(input,output){
     }, ignoreNULL = FALSE)
     output$table <- renderDataTable( df())
     
-    ##### Build model and make predictions
-    # 
-    # pred_matrix <- reactiveValues(ratingMatrix = reshape2::dcast(rating_data,
-    #                                                              userId~movieId,
-    #                                                              value.var = "rating",
-    #                                                              na.rm=FALSE) %>%
-    #                                   #select(-c('userId')) %>% # same as row index, just taking up space
-    #                                   as.matrix() %>% #needs to be matrix to convert to sparse matrix
-    #                                   as(.,"realRatingMatrix"))
-    # 
-    # movie_ratings <- reactiveValues(ratingMatrix <- pred_matrix$ratingMatrix[rowCounts(ratingMatrix) > 0,
-    #                                                                          colCounts(ratingMatrix) > 50])
-    # 
-    # e <-  reactiveValues(eval_sheme <- evaluationScheme(movie_ratings$ratingMatrix, #evaluate will normalize for us
-    #                                                     method="cross", 
-    #                                                     k = 3,#3 fold cross validation
-    #                                                     given=3, 
-    #                                                     goodRating=3))
-    # 
-    # rec_model <- reactiveValues(rec <- Recommender(data = getData(e$eval_sheme,"train"),
-    #                                                method = "POPULAR"))
-    # 
-    # top_recommendations <- reactiveValues(j = 10) # the number of items to recommend to each user
-    # 
-    # predicted_recommendations <- reactiveValues(pred <- predict(object = rec_model$rec,
-    #                                                             newdata = getData(e$eval_sheme,"known"),
-    #                                                             n = top_recommendations$j))
-    # 
-    # # 10 recommendations for the first user
-    # user1 <- reactiveValues(pred <- predicted_recommendations$pred@items[[669]])
-    # movies_user1 <- reactiveValues(a <- predicted_recommendations$pred@itemLabels[user1$pred])
-    # movies_user2 <- reactiveValues(one <- movies_user1)
-    # for (index in 1:10){
-    #     movies_user2$one[index] <- as.character(
-    #         subset(movie_data,movie_data$movieId == movies_user1[index])$title)
-    # }
-    # movies_user2
+    
+    ######################################### Page two #########################
+    
+
+        pred <- eventReactive(input$pred_submit, {
+        rating_data <- df() %>% as_tibble()
+        
+        #### reshape data
+        movie_ratings <- reshape2::dcast(rating_data,
+                                         userId~movieId,
+                                         value.var = "rating",
+                                         na.rm=FALSE) %>%
+            select(-c('userId')) %>% # same as row index, just taking up space
+            as.matrix() %>% #needs to be matrix to convert to sparse matrix
+            as(.,"realRatingMatrix")
+        
+        ####splitting data
+        mid = as.numeric(input$train_submit) * dim(movie_ratings)[1]
+        mid = if_else(floor(mid) == mid, mid, floor(mid))
+        train <- movie_ratings[1:mid]
+        test <- movie_ratings[(mid+1):dim(movie_ratings)[1]]
+        
+        ##### Build model and make predictions
+        if (input$model == "Popular"){
+            param_list = list()
+        } else if (input$model == "ALS"){
+            param_list = list(n_factors = 40)
+        } else if (input$model == "UBCF"){
+            param_list = list(method = "Cosine", nn = 1)
+        } else if (input$model == "SVD"){
+            param_list = list(k = 10)
+        }
+        rec_model <- Recommender(data = train,
+                                 method = as.character(input$model),
+                                 param = param_list)
+
+        User_pred <- if_else(as.numeric(input$USERID) > mid,
+                             as.numeric(input$USERID) - mid,
+                             as.numeric(input$USERID))
+        
+        movies_user <- as(predict(rec_model,
+                                  User_pred,#this is the user ID
+                                  data = test),
+                          "list")
+        for (index in 1:10){
+            movies_user[index] <- as.character(
+                subset(movie_data,movie_data$movieId == movies_user1[index])$title)
+        }
+
+        #print nice table
+        #how do i get the x out of the colname spot???
+        M <- movies_user %>% as.matrix()
+        M <- print(xtable(M),
+                   floating = F, tabular.environment = "array", comment = F, print.results = F)
+        html <- paste0("$$",M,"$$")
+        html %>%
+            HTML() %>%
+            withMathJax() %>%
+            list()
+    })
+    
+    output$pred <- renderUI( pred())
+    
+   
 }
 
 shinyApp(ui,server)
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # 2021_Summer_Research
 
-View knitted Rmarkdown file at https://rpubs.com/Sean_Cranston/Movie_reco.
+
+This undergraduate research project was conducted by Sean Cranston under the supervision
+of Dr. Mengshi Zhou at St. Cloud State University. After deciding to do our study on
+Movie Recommenders we looked at many different articles on the topic. Next we began to study
+the r packages manuals. In particular, we spent a great deal of time studying the recommenderlab
+package. Through the different articles we explored how to preprocess data, visualize data, 
+fit many models, hyperparameter
+ optimization, cross-validation, model comparison, ROC curves, Precision-Recall curves, 
+ and compare with random selection. 
+ furthermore, we deployed a shiny app to where you can add data (rate some movies)
+ and play with the different times of models (only the best models).
+ 
+
+To view knitted Rmarkdown file go to https://rpubs.com/Sean_Cranston/Movie_reco.
+
+To view the shiny app go to https://sean-cranston.shinyapps.io/Movie_recommender/
+
+This project was a fun challenge and I am grateful for Dr. Zhou's guidance
